8b3d4818f7e4ec564222256ad93981ff5f4b2cd5
richardnoel1337 <richardnoel1337@gmail.com>
2020-10-14 13:16:39 -0400

    added slight html for the navbar

--- BlurbClient/src/app/app-routing.module.ts
+++ BlurbClient/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
-  // { path:"", component:HomeComponent },
+  // { path:"", component:NavbarComponent },
   { path:"user", component:UserComponent }
 ];
 
